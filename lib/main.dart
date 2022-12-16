cfb6ec63444c25ca15de7b6399b3d5032dd4eff9
Diwash Rai <raidiwash30@gmail.com>
2022-12-16 09:12:33 +0545

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,78 +11,112 @@ class AboutPage extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
-            appBar: AppBar(
-              title: const Text(
-                'About Page',
-              ),
-              actions: const [
-                Icon(
-                  Icons.home,
-                  color: Colors.white,
-                ),
-              ],
-              leading: Builder(builder: (ctx) {
-                return IconButton(
+          appBar: AppBar(
+            title: const Text(
+              'About Page',
+            ),
+            actions: [
+              Builder(builder: (context) {
+                return ElevatedButton(
                   onPressed: () {
-                    ///open drawer using scafffold
-                    Scaffold.of(ctx).openDrawer(); // ctx=context
+                    Scaffold.of(context).openEndDrawer();
                   },
-                  icon: const Icon(
-                    Icons.menu,
+                  child: const Icon(
+                    Icons.home,
                     color: Colors.white,
                   ),
                 );
               }),
-            ),
-            body: Center(
-              child: Text(
-                'Center Text',
-              ),
-            ),
-            drawer: Drawer(
-              child: ListView(
-                children: [
-                  Icon(
-                    Icons.image,
-                    color: Colors.red,
-                    size: 50,
-                  ),
-                  Text(
-                    'Full Name',
-                  ),
-                  Text(
-                    'Address',
-                  ),
-                  Divider(
-                    color: Colors.black,
-                  ),
-                  Card(
-                    child: ListTile(
-                      title: Text(
-                        'First Menu',
-                      ),
-                    ),
-                  ),
-                  Card(
-                    child: ListTile(
-                      title: Text(
-                        'Second Menu',
-                      ),
-                    ),
-                  ),
-                  Card(
-                    child: ListTile(
-                      title: Text(
-                        'Third Menu',
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            onDrawerChanged: (val) {
-              print(val);
+            ],
+            leading: Builder(builder: (ctx) {
+              return IconButton(
+                onPressed: () {
+                  ///open drawer using scafffold
+                  Scaffold.of(ctx).openDrawer(); // ctx=context
+                },
+                icon: const Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                ),
+              );
             }),
+          ),
+          body: Center(
+            child: Text(
+              'Center Text',
+            ),
+          ),
+          drawer: Drawer(
+            child: ListView(
+              children: [
+                Icon(
+                  Icons.image,
+                  color: Colors.red,
+                  size: 50,
+                ),
+                Builder(builder: (context) {
+                  return ElevatedButton(
+                      onPressed: () {
+                        Scaffold.of(context).closeDrawer();
+                      },
+                      child: Icon(Icons.close));
+                }),
+                Text(
+                  'Full Name',
+                ),
+                Text(
+                  'Address',
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                Card(
+                  child: ListTile(
+                    title: Text(
+                      'First Menu',
+                    ),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Text(
+                      'Second Menu',
+                    ),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Text(
+                      'Third Menu',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          onDrawerChanged: (val) {
+            print(val);
+          },
+          endDrawer: Drawer(
+              child: ListView(
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text("Diwash"),
+                accountEmail: Text("diwsh@gmail.com"),
+                currentAccountPicture: Icon(Icons.circle),
+              )
+            ],
+          )),
+          persistentFooterButtons: [
+            OutlinedButton(onPressed: () {}, child: Text("home")),
+            OutlinedButton(onPressed: () {}, child: Text("about")),
+            OutlinedButton(onPressed: () {}, child: Text("account"))
+          ],
+          bottomNavigationBar: Container(
+            height: 80,
+            color: Colors.blue,
+          ),
+        ),
       ),
     );
   }
