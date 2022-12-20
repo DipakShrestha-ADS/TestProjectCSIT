1a29ca73a8e9e497dadb6a9868f929bdf6c1d1d7
Diwash Rai <raidiwash30@gmail.com>
2022-12-20 08:14:16 +0545

    new file added

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    final Size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+                onTap: () {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("on tab"),
+                    duration: Duration(seconds: 1),
+                  ));
+                },
+                child: Icon(Icons.settings)),
+            Icon(Icons.start)
+          ],
+        ),
+        shape: CircularNotchedRectangle(),
+        notchMargin: 20,
+      ),
+      floatingActionButton: Builder(
+        builder: (c) {
+          return FloatingActionButton(
+            onPressed: () {
+              showBottomSheet(
+                  context: c,
+                  builder: (c) {
+                    return Container(
+                      color: Colors.blue,
+                      height: 250,
+                      width: Size.width,
+                      child: Text("dialog"),
+                    );
+                  });
+            },
+            child: Icon(Icons.home),
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:test_project/home.dart';
 
 void main() {
-  runApp(AboutPage());
+  runApp(MaterialApp(
+    home: myhome(),
+  ));
 }
 
-class AboutPage extends StatelessWidget {
+class myhome extends StatefulWidget {
+  const myhome({super.key});
+
+  @override
+  State<myhome> createState() => _myhomeState();
+}
+
+class _myhomeState extends State<myhome> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              'About Page',
-            ),
-            actions: [
-              Builder(builder: (context) {
-                return ElevatedButton(
-                  onPressed: () {
-                    Scaffold.of(context).openEndDrawer();
-                  },
-                  child: const Icon(
-                    Icons.home,
-                    color: Colors.white,
-                  ),
-                );
-              }),
-            ],
-            leading: Builder(builder: (ctx) {
-              return IconButton(
-                onPressed: () {
-                  ///open drawer using scafffold
-                  Scaffold.of(ctx).openDrawer(); // ctx=context
-                },
-                icon: const Icon(
-                  Icons.menu,
-                  color: Colors.white,
-                ),
-              );
-            }),
-          ),
-          body: Center(
-            child: Text(
-              'Center Text',
-            ),
-          ),
-          drawer: Drawer(
-            child: ListView(
-              children: [
-                Icon(
-                  Icons.image,
-                  color: Colors.red,
-                  size: 50,
-                ),
-                Builder(builder: (context) {
-                  return ElevatedButton(
-                      onPressed: () {
-                        Scaffold.of(context).closeDrawer();
-                      },
-                      child: Icon(Icons.close));
-                }),
-                Text(
-                  'Full Name',
-                ),
-                Text(
-                  'Address',
-                ),
-                Divider(
-                  color: Colors.black,
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text(
-                      'First Menu',
-                    ),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text(
-                      'Second Menu',
-                    ),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text(
-                      'Third Menu',
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          onDrawerChanged: (val) {
-            print(val);
-          },
-          endDrawer: Drawer(
-              child: ListView(
-            children: [
-              UserAccountsDrawerHeader(
-                accountName: Text("Diwash"),
-                accountEmail: Text("diwsh@gmail.com"),
-                currentAccountPicture: Icon(Icons.circle),
-              )
-            ],
-          )),
-          persistentFooterButtons: [
-            OutlinedButton(onPressed: () {}, child: Text("home")),
-            OutlinedButton(onPressed: () {}, child: Text("about")),
-            OutlinedButton(onPressed: () {}, child: Text("account"))
-          ],
-          bottomNavigationBar: Container(
-            height: 80,
-            color: Colors.blue,
-          ),
-        ),
-      ),
+    return Scaffold(
+      body: Home(),
     );
   }
 }
+
+// class AboutPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: SafeArea(
+//         child: Scaffold(
+//           appBar: AppBar(
+//             title: const Text(
+//               'About Page',
+//             ),
+//             actions: [
+//               Builder(builder: (context) {
+//                 return ElevatedButton(
+//                   onPressed: () {
+//                     Scaffold.of(context).openEndDrawer();
+//                   },
+//                   child: const Icon(
+//                     Icons.home,
+//                     color: Colors.white,
+//                   ),
+//                 );
+//               }),
+//             ],
+//             leading: Builder(builder: (ctx) {
+//               return IconButton(
+//                 onPressed: () {
+//                   ///open drawer using scafffold
+//                   Scaffold.of(ctx).openDrawer(); // ctx=context
+//                 },
+//                 icon: const Icon(
+//                   Icons.menu,
+//                   color: Colors.white,
+//                 ),
+//               );
+//             }),
+//           ),
+//           body: Center(
+//             child: Text(
+//               'Center Text',
+//             ),
+//           ),
+//           drawer: Drawer(
+//             child: ListView(
+//               children: [
+//                 Icon(
+//                   Icons.image,
+//                   color: Colors.red,
+//                   size: 50,
+//                 ),
+//                 Builder(builder: (context) {
+//                   return ElevatedButton(
+//                       onPressed: () {
+//                         Scaffold.of(context).closeDrawer();
+//                       },
+//                       child: Icon(Icons.close));
+//                 }),
+//                 Text(
+//                   'Full Name',
+//                 ),
+//                 Text(
+//                   'Address',
+//                 ),
+//                 Divider(
+//                   color: Colors.black,
+//                 ),
+//                 Card(
+//                   child: ListTile(
+//                     title: Text(
+//                       'First Menu',
+//                     ),
+//                   ),
+//                 ),
+//                 Card(
+//                   child: ListTile(
+//                     title: Text(
+//                       'Second Menu',
+//                     ),
+//                   ),
+//                 ),
+//                 Card(
+//                   child: ListTile(
+//                     title: Text(
+//                       'Third Menu',
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           onDrawerChanged: (val) {
+//             print(val);
+//           },
+//           endDrawer: Drawer(
+//               child: ListView(
+//             children: [
+//               UserAccountsDrawerHeader(
+//                 accountName: Text("Diwash"),
+//                 accountEmail: Text("diwsh@gmail.com"),
+//                 currentAccountPicture: Icon(Icons.circle),
+//               )
+//             ],
+//           )),
+//           persistentFooterButtons: [
+//             OutlinedButton(onPressed: () {}, child: Text("home")),
+//             OutlinedButton(onPressed: () {}, child: Text("about")),
+//             OutlinedButton(onPressed: () {}, child: Text("account"))
+//           ],
+//           bottomNavigationBar: Container(
+//             height: 80,
+//             color: Colors.blue,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
