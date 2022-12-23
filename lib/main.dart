0f4e275ce0a72378edba13e9eb174e81bfda571d
DipakShrestha-ADS <dipak.shrestha@eemc.edu.np>
2022-12-23 08:16:16 +0545

    container example

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_project/home.dart';
+import 'package:test_project/widgets/custom_container.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,148 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
-    );
-  }
-}
-
-class AboutPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
-            'About Page',
-          ),
-          actions: [
-            Builder(
-              builder: (ctx) {
-                return IconButton(
-                  onPressed: () {
-                    Scaffold.of(ctx).openEndDrawer();
-                  },
-                  icon: Icon(
-                    Icons.menu_book,
-                  ),
-                );
-              },
-            ),
-          ],
-          leading: Builder(builder: (ctx) {
-            return IconButton(
-              onPressed: () {
-                ///open drawer using scafffold
-                Scaffold.of(ctx).openDrawer();
-              },
-              icon: Icon(
-                Icons.menu,
-                color: Colors.white,
-              ),
-            );
-          }),
+          title: Text('Widgets'),
         ),
-        body: Center(
-          child: Text(
-            'Center Text',
-          ),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(
-                    Icons.image,
-                    color: Colors.red,
-                    size: 50,
-                  ),
-                  Builder(builder: (ctx) {
-                    return IconButton(
-                      onPressed: () {
-                        Scaffold.of(ctx).closeDrawer();
-                      },
-                      icon: Icon(
-                        Icons.close,
-                        color: Colors.red,
-                      ),
-                    );
-                  })
-                ],
-              ),
-              Text(
-                'Full Name',
-              ),
-              Text(
-                'Address',
-              ),
-              Divider(
-                color: Colors.black,
-              ),
-              Card(
-                child: ListTile(
-                  title: Text(
-                    'First Menu',
-                  ),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  title: Text(
-                    'Second Menu',
-                  ),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  title: Text(
-                    'Third Menu',
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        onDrawerChanged: (val) {
-          print('on drawer change: $val');
-        },
-        endDrawer: Drawer(
-          child: ListView(
-            children: [
-              UserAccountsDrawerHeader(
-                accountName: Text(
-                  'Account Name',
-                ),
-                accountEmail: Text(
-                  'youremail@gmail.com',
-                ),
-                currentAccountPicture: CircleAvatar(
-                  child: Text('Image'),
-                ),
-              ),
-            ],
-          ),
-        ),
-        persistentFooterButtons: [
-          OutlinedButton(
-            onPressed: () {},
-            child: Text('Home'),
-          ),
-          OutlinedButton(
-            onPressed: () {},
-            child: Text('Profile'),
-          ),
-          OutlinedButton(
-            onPressed: () {},
-            child: Text('Setting'),
-          ),
-        ],
-        bottomNavigationBar: Container(
-          height: 80,
-          color: Colors.blue,
-        ),
+        body: CustomContainer(),
       ),
     );
   }
---
+++ lib/widgets/custom_container.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.horizontal(
+          left: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            offset: Offset(10, 10),
+            blurRadius: 10,
+          ),
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            offset: Offset(20, 30),
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      child: Text(
+        'Container',
+        style: TextStyle(
+          fontSize: 40,
+          color: Colors.white,
+        ),
+        textAlign: TextAlign.right,
+      ),
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.only(
+        top: 20,
+        left: 20,
+      ),
+    );
+  }
+}
